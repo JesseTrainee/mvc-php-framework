4356eeacd86f8eb8c862cbdb16e932397d55be2a
JesseTrainee <a.l.i.t.t.l.e@hotmail.com>
2022-01-10 14:52:13 -0300

    creating bootstrap abstract class

--- App/Route.php
+++ App/Route.php
@@ -1,24 +1,12 @@
 <?php
+
 namespace App;
 
-class Route {
+use MF\Init\Bootstrap;
 
-    private $routes;
-
-    public function __construct() {
-        $this->initRoute();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes() {
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes) {
-        $this->routes = $routes;
-    }
+class Route extends Bootstrap {
     
-    public function initRoute() {
+    public function initRoutes() {
         $routes['home'] = array(
             'route' => '/',
             'controller' => 'indexController',
@@ -32,24 +20,6 @@ class Route {
         );
 
         $this->setRoutes($routes);
-    }
-
-    public function run($url) {
-        foreach($this->getRoutes() as $key => $route) {
-            if($url == $route['route']) {
-                $class = "App\\Controllers\\" . ucfirst($route['controller']);
-
-                $controller = new $class;
-
-                $action = $route['action'];
-
-                $controller->$action();
-            }
-        }
-    }
-
-    public function getUrl() {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 }
 ?>
